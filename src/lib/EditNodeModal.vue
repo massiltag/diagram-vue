27546979204d677f38071670077a49436d334270
Massil Taguemout <massiltag@icloud.com>
2021-05-26 03:09:16 +0200

    add operations to class creation form

--- src/lib/EditNodeModal.vue
+++ src/lib/EditNodeModal.vue
@@ -10,7 +10,6 @@
         <VSelect v-model="newNode.type" placeholder="Select type">
           <option value="class" selected>Class</option>
           <option value="interface">Interface</option>
-
         </VSelect>
         <br/>
 
@@ -31,6 +30,47 @@
                 </VSelect>
               </td>
               <td><VButton @click="deleteAttribute(a.name)">delete</VButton></td>
+            </tr>
+          </table>
+        </div>
+
+        <div class="methods" style="margin-top: 20px">
+          <h2 style="display: inline; margin: 10px 10px 10px 0">Methods</h2>
+          <VButton style="display: inline" @click="newMethod">New</VButton>
+          <table v-if="methodList.length > 0">
+            <tr>
+              <th>UML Method</th>
+              <th>Return type</th>
+              <th>Params</th>
+              <th>Actions</th>
+              <th>Add parameter</th>
+            </tr>
+            <tr v-for="a in methodList">
+              <td><input type="text" v-model="a.name"></td>
+              <td>
+                <VSelect v-model="a.type" placeholder="Select type">
+                  <option v-for="t in attributeTypes" :value="t">{{ t }}</option>
+                </VSelect>
+              </td>
+              <td width="180">
+                <span style="font-size: 15px" v-for="p in a.params">{{ p.name }}: {{ p.type }}, </span>
+              </td>
+              <td>
+                <VButton @click="deleteMethod(a.name)">delete</VButton>
+                <!--VButton @click="toggleAddParam(a.name)">add param</VButton-->
+                <VButton @click="clearMethodParams(a.name)">clear params</VButton>
+              </td>
+              <!--td>
+                <div class="add-param" v-if="a.addParam">
+                  <input type="text" class="inline" style="width: 30%" v-model="tempParam.name" placeholder="Param name">
+
+                  <VSelect class="inline" v-model="tempParam.type" placeholder="Select type">
+                    <option v-for="t in attributeTypes" :value="t">{{ t }}</option>
+                  </VSelect>
+
+                  <VButton class="inline" @click="addMethodParam(a.name)">add param</VButton>
+                </div>
+              </td-->
             </tr>
           </table>
         </div>
@@ -73,8 +113,9 @@
 
 <script lang="js">
 
-import UMLProperty from "@/lib/model/UMLProperty";
+import UMLAttribute from "@/lib/model/UMLElements";
 import VButton from "@/minimal-ui/lib/VButton";
+import UMLMethod from "@/lib/model/UMLElements";
 
 export default {
   components: { VButton },
@@ -95,17 +136,32 @@ export default {
             url: "",
             color: "#ecf0f1"
           },
-          type: "class"
+          type: "class",
+          attributes: Array,
+          methods: Array,
         };
       }
     },
     attributeList: {
-      type: Array(UMLProperty),
+      type: Array,
       default() {
         return [
           {
-            name: 'Test',
+            name: 'test',
             type: 'String'
+          }
+        ]
+      }
+    },
+    methodList: {
+      type: Array,
+      default() {
+        return [
+          {
+            name: 'op1',
+            type: 'String',
+            params: [],
+            addParam: false
           }
         ]
       }
@@ -115,52 +171,92 @@ export default {
       default() {
         return ['Number', 'String', 'Boolean'];
       }
-    }
+    },
+
   },
   watch: {
     node() {
       this.newWidth = parseInt(this.node.width);
       this.newHeight = parseInt(this.node.height);
+    },
+    attributeList() {
+      this.newNode.attributes = this.attributeList;
+    },
+    methodList() {
+      this.newNode.methods = this.methodList;
     }
   },
   data() {
     return {
-      newNode: this.node
+      newNode: this.node,
+
+      tempParam: {
+        name: '',
+        type: ''
+      }
     };
   },
   methods: {
+    init() {
+      this.attributeList = [];
+    },
     ok() {
       this.$emit("ok", this.newNode);
     },
     cancel() {
       this.$emit("cancel");
     },
+
+    // Attributes
+    newAttribute() {
+      this.attributeList.push(new UMLAttribute("", "String"))
+    },
     deleteAttribute(name) {
       this.attributeList = this.attributeList.filter(a => a.name !== name)
     },
-    newAttribute() {
-      this.attributeList.push(new UMLProperty("", ""))
+
+    // Methods
+    newMethod() {
+      this.methodList.push(new UMLMethod("", "String", []))
+    },
+    clearMethodParams(name) {
+      /* TODO take something else in consideration than the name (like the index),
+         we can't overload methods with this solution, deleting props will affect all the methods which have the same name */
+      this.methodList.map(a => { if (a.name === name) a.params = [] })
+    },
+    addMethodParam(methodName) {
+      this.methodList.map(a => { if (a.name === methodName && this.tempParam.name && this.tempParam.type) a.params.push(this.tempParam) })
+    },
+    toggleAddParam(methodName) {
+      this.methodList.map(a => a.addParam = (a.name === methodName))
+    },
+    deleteMethod(name) {
+      this.methodList = this.methodList.filter(a => a.name !== name)
     }
   }
 };
 
 </script>
 <style lang="scss" scoped>
-input {
-  width: 95%;
-  margin-bottom: 5px;
-}
-select {
-  margin-bottom: 5px;
-}
-.item-enter-active {
-  transition: all 0.8s ease;
-}
-.item-leave-active {
-  transition: all 0.3s ease;
-}
-.item-enter,
-.item-leave-to {
-  opacity: 0;
-}
+  input {
+    width: 95%;
+    margin-bottom: 5px;
+  }
+  select {
+    margin-bottom: 5px;
+  }
+  .item-enter-active {
+    transition: all 0.8s ease;
+  }
+  .item-leave-active {
+    transition: all 0.3s ease;
+  }
+  .item-enter,
+  .item-leave-to {
+    opacity: 0;
+  }
+
+  .inline {
+    display: inline;
+  }
 </style>
